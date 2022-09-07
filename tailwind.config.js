80b6cedb71357ff7666309c41eb7d775a890be52
Diego Ricardo <diegoricardoweb@gmail.com>
2022-09-07 19:12:29 -0300

    Criado componente Banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line no-undef
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  // eslint-disable-next-line no-undef
+  plugins: [require('tailwindcss-textshadow')]
 }
