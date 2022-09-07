56eae455010cac00504cab7de21ab8b9c8b9581a
Diego Ricardo <diegoricardoweb@gmail.com>
2022-09-07 16:44:40 -0300

    Criado componente Header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,5 +1,65 @@
+import { BellIcon, SearchIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+
 function Header() {
-  return <div>Header</div>
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  })
+
+  return (
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex items-center space-x-2 md:space-x-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100"
+          height="27"
+          viewBox="0 0 1024 276.742"
+          role="img"
+        >
+          <path
+            fill="#d81f26"
+            d="M140.803 258.904c-15.404 2.705-31.079 3.516-47.294 5.676L44.051 119.724v151.073C28.647 272.418 14.594 274.58 0 276.742V0h41.08l56.212 157.021V0h43.511v258.904zm85.131-157.558c16.757 0 42.431-.811 57.835-.811v43.24c-19.189 0-41.619 0-57.835.811v64.322c25.405-1.621 50.809-3.785 76.482-4.596v41.617l-119.724 9.461V0h119.724v43.241h-76.482v58.105zm237.284-58.104h-44.862V242.15c-14.594 0-29.188 0-43.239.539V43.242h-44.862V0H463.22l-.002 43.242zm70.266 55.132h59.187v43.24h-59.187v98.104h-42.433V0h120.808v43.241h-78.375v55.133zm148.641 103.507c24.594.539 49.456 2.434 73.51 3.783v42.701c-38.646-2.434-77.293-4.863-116.75-5.676V0h43.24v201.881zm109.994 49.457c13.783.812 28.377 1.623 42.43 3.242V0h-42.43v251.338zM1024 0l-54.863 131.615L1024 276.742c-16.217-2.162-32.432-5.135-48.648-7.838l-31.078-79.994-31.617 73.51c-15.678-2.705-30.812-3.516-46.484-5.678l55.672-126.75L871.576 0h46.482l28.377 72.699L976.705 0H1024z"
+          ></path>
+        </svg>
+
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Filmes</li>
+          <li className="headerLink">Novos & Populares</li>
+          <li className="headerLink">Minha Lista</li>
+        </ul>
+      </div>
+
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
+    </header>
+  )
 }
 
 export default Header
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
+const nextConfig = {
+  reactStrictMode: true
 }
+
+// eslint-disable-next-line no-undef
+module.exports = nextConfig
