ba08fa16f12d7e6915c30acc09563202a7b2cce5
Diego Ricardo <diegoricardoweb@gmail.com>
2022-09-08 00:54:32 -0300

    Criado componente Row

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 
 // eslint-disable-next-line no-undef
@@ -7,8 +8,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'gradient-to-b':
+          'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);'
+      }
+    }
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require('tailwindcss-textshadow')]
+  plugins: [
+    require('tailwindcss-textshadow'),
+    require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar')
+  ]
 }
