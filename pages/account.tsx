b963f89055c06ff724c70c2a30feea7d24b97d1e
Diego Ricardo <diegoricardoweb@gmail.com>
2022-09-17 17:17:57 -0300

    Criado pagina Account

---
+++ pages/account.tsx
@@ -0,0 +1,110 @@
+import { getProducts, Product } from '@stripe/firestore-stripe-payments'
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
+import Membership from '../components/Membership'
+import useAuth from '../hooks/useAuth'
+import useSubscription from '../hooks/useSubscription'
+import payments from '../lib/stripe'
+
+interface Props {
+  products: Product[]
+}
+
+function Account({ products }: Props) {
+  const { user, logout } = useAuth()
+  const subscription = useSubscription(user)
+
+  console.log('Produto', products)
+  console.log('Subscription', subscription)
+
+  return (
+    <div>
+      <Head>
+        <title>Configurações da Conta - Netflix Clone</title>
+        <link
+          rel="shortcut icon"
+          href="https://assets.nflxext.com/ffe/siteui/common/icons/nficon2016.ico"
+        />
+      </Head>
+
+      <header className={`bg-[#141414]`}>
+        <Link href="/">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+            width={120}
+            height={120}
+            className="cursor-pointer object-contain"
+          />
+        </Link>
+        <Link href="/account">
+          <img
+            src="https://occ-0-1190-2774.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </header>
+
+      <main className="mx-auto max-w-6xl px-5 pt-24 pb-12 transition-all md:px-10">
+        <div className="flex flex-col gap-x-4 md:flex-row md:items-center">
+          <h1 className="text-3xl md:text-4xl">Account</h1>
+
+          <div className="-ml-0.5 flex items-center gap-x-1.5">
+            <img
+              src="https://assets.nflxext.com/ffe/siteui/account/svg/membersince.svg"
+              alt=""
+              className="h-7 w-7"
+            />
+            <p className="text-xs font-semibold text-[#555]">
+              Membro desde {subscription?.created}
+            </p>
+          </div>
+        </div>
+
+        <Membership />
+
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0 md:pb-0">
+          <h4 className="text-lg text-[gray]">Detalhes do Plano</h4>
+          <div className="col-span-2 font-medium">
+            {
+              products?.filter(
+                (product) => product.id === subscription?.product
+              )[0]?.name
+            }
+          </div>
+          <p className="cursor-pointer text-blue-500 hover:underline md:text-right">
+            Alterar plano
+          </p>
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-x-4 border px-4 py-4 md:grid-cols-4 md:border-x-0 md:border-t md:border-b-0 md:px-0">
+          <h4 className="text-lg text-[gray]">Configurações</h4>
+          <p
+            className="col-span-3 cursor-pointer text-blue-500 hover:underline"
+            onClick={logout}
+          >
+            Sair de todos os dispositivos
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Account
+
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await getProducts(payments, {
+    includePrices: true,
+    activeOnly: true
+  })
+    .then((res) => res)
+    .catch((error) => console.log(error.message))
+
+  return {
+    props: {
+      products
+    }
+  }
+}
